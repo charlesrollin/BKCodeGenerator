736a770674168be37a8b86bcb79766023643043b
Charles Rollin <charles.rollin@illuin.tech>
2018-11-15 10:56:59 +0100

    feat(options): allow to choose between slow and fast mode

--- app/index.js
+++ app/index.js
@@ -10,11 +10,11 @@ const {
 
 const { handleSelects } = require("./scrappers/Selects");
 
-async function example(bkCode) {
+async function example(bkCode, slowMode) {
+  const startDate = Date.now();
   if (bkCode) {
     setBKCode(bkCode);
   }
-  const stdin = process.stdin;
   let driver = await new Builder().forBrowser("safari").build();
   await driver.manage().setTimeouts({ implicit: 5000 });
   let keepGoing = true;
@@ -23,24 +23,16 @@ async function example(bkCode) {
     await driver.get("https://www.bkvousecoute.fr/");
     while (keepGoing) {
       console.log(`Entering step ${stepCount}`);
-      keepGoing = await handleStep(driver);
+      // Go slow if the user aksed to or if this is the 2nd step
+      keepGoing = await handleStep(driver, slowMode || stepCount === 1);
       stepCount++;
     }
     console.log(await getCodeString(driver));
   } catch (err) {
     console.log(err);
   } finally {
-    console.log("Press enter to quit the script:");
-    stdin.resume();
-    stdin.on("data", () => end(driver));
-  }
-}
-
-async function end(driver) {
-  try {
+    console.log(`Finished in ${(Date.now() - startDate) / 1000}`);
     await driver.quit();
-  } finally {
-    process.exit();
   }
 }
 
@@ -58,14 +50,18 @@ async function getCodeString(driver) {
   return await driver.findElement(By.className("ValCode")).getText();
 }
 
-async function handleStep(driver) {
-  await handleRadioInputsInSpan(driver);
-  await handleRadioInputsInTable(driver);
-  await handleSelects(driver);
-  await handleCheckboxes(driver);
-  // Text inputs should come last: some checkboxes / radios may show additional inputs on click
-  await handleTextInputs(driver);
+async function handleStep(driver, isSlowMode) {
+  if (isSlowMode) {
+    await handleRadioInputsInSpan(driver);
+    await handleRadioInputsInTable(driver);
+    await handleSelects(driver);
+    await handleCheckboxes(driver);
+    // Text inputs should come last: some checkboxes / radios may show additional inputs on click
+    await handleTextInputs(driver);
+  }
   return await goToNextStep(driver);
 }
 
-example(...process.argv.slice(2));
+const argv = require('minimist')(process.argv.slice(2));
+
+example(argv.code, argv.slowMode);
